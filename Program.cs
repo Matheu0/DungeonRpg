1a04d3c69e9b046bd4fdce921abfe700fb4eec4a
Matheus <matheuslessa05@hotmail.com>
2022-05-29 20:49:13 -0300

    Add New Status and Displaying the battle

--- Program.cs
+++ Program.cs
@@ -7,11 +7,24 @@ namespace DungeonRpg
     {
         static void Main(string[] args)
         {
-          Hero Arus = new Hero("Arus", 23, "Knight");
-          Wizard Wizard = new Wizard("Jennica", 23, "White Wizard");
+          Knight Arus = new Knight("Arus", 23, "Knight", 200, 30);
+          Wizard Jenica = new Wizard("Jennica", 23, "White Wizard,", 100, 80);
+          BlackWizard Topapa = new BlackWizard("Topapa", 24, "Black Wizard", 110, 90);
 
-          Console.WriteLine(Arus.Attack());
-          Console.WriteLine(Wizard.Attack(1));
+          Console.WriteLine("Os Dados rolam, Sua Party atacou: ");
+          
+          Console.WriteLine(Arus.Attack());          
+
+          Console.WriteLine(Jenica.Attack());
+          Console.WriteLine(Jenica.MageAttack(1));
+          Console.WriteLine(Jenica.MageAttack(7));
+
+          Console.WriteLine(Topapa.Attack());
+          Console.WriteLine(Topapa.MageAttack(1));
+          Console.WriteLine(Topapa.MageAttack(10));
+
+
+
         }
     }
 }
