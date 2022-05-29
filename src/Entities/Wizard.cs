cab99272d3cabbccc5ba878498de22a302230e9d
Matheus <matheuslessa05@hotmail.com>
2022-05-29 19:46:01 -0300

    Create Class "Hero" and "Wizard"

---
+++ src/Entities/Wizard.cs
@@ -0,0 +1,18 @@
+namespace DungeonRpg.src.Entities
+{
+    public class Wizard : Hero
+    {
+        public Wizard(string Name, int Level, string HeroType) : base(Name, Level, HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+        public override string Attack()
+        {
+            return this.Name + " Lançou Bola de fogo";
+        } 
+    }
+
+    }
